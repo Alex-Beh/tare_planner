6008d2eca8074eccdb47ae9ffc5bf03a50def120
Alex-Beh <chunjye088@Gmail.com>
2023-08-22 23:17:00 +0800

    use MultiThreadedExecutor for ros2 node spin

--- src/tare_planner/src/tare_planner_node/tare_planner_node.cpp
+++ src/tare_planner/src/tare_planner_node/tare_planner_node.cpp
@@ -6,7 +6,9 @@ int main(int argc, char** argv)
   rclcpp::init(argc, argv);
   auto node = std::make_shared<sensor_coverage_planner_3d_ns::SensorCoveragePlanner3D>();
   node->initialize();
-  rclcpp::spin(node);
+  rclcpp::executors::MultiThreadedExecutor executor;
+  executor.add_node(node);
+  executor.spin();
   rclcpp::shutdown();
   return 0;
 }
